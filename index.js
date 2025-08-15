2390e720e80992310f3f06c064e8c08c705a62bf
Dharmik Umretiya <dharmikcode@gmail.com>
2025-08-15 16:02:43 +0530

    Added Proxy code

---
+++ index.js
@@ -0,0 +1,21 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server, path: '/ws' });
+
+
+wss.on('connection', function connection(clientWs, req) {
+  
+  const espWs = new WebSocket("ws://192.168.4.1/ws");
+
+  espWs.on('open', () => console.log('Connected to ESP32'));
+  espWs.on('message', (msg) => clientWs.send(msg));   // ESP32 -> Frontend
+  clientWs.on('message', (msg) => espWs.send(msg));   // Frontend -> ESP32
+});
+
+const PORT = process.env.PORT || 443;
+server.listen(PORT, () => {
+  console.log(`WSS proxy running on port ${PORT}`);
+});
